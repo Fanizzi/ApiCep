1b7638b7f101eab8b5aee562014ce21e1096a279
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-02-14 16:12:35 -0300

    first commit

---
+++ Controller/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ApiCep\Controller;
+
+abstract class Controller
+{
+    protected static function getResponseAsJSON($data)
+    {
+        header("Acess-Control-Allow-Origin: *");
+        header("Content-type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");
+
+        exit(json_encode($data));
+    }
+}
