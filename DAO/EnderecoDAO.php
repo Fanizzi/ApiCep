a14001f6b37f32d5ee3a04c9dc3d2927fd4d539d
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-03-07 16:09:34 -0300

    add EnderecoDAO

---
+++ DAO/EnderecoDAO.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace ApiCep\DAO;
+
+use ApiCep\Model\EnderecoModel;
+
+class EnderecoDAO extends DAO
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function selectCidadesByUf($uf)
+    {
+        $sql = "SELECT * FROM cidade WHERE uf = ? ORDER BY descricao";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $uf);
+        $stmt->execute();
+    }
+
+    public function selectByCep(int $cep)
+    {
+        $sql = "SELECT * FROM logradouro WHERE cep = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $cep);
+        $stmt->execute();
+
+        $endereco_obj = $stmt->fetchObject("App\Model\EnderecoModel");
+
+        $endereco_obj->arr_cidades = $this->selectCidadesByUf($endereco_obj->UF);
+
+        return $endereco_obj;
+    }
+
+    public function selectLogradouroByBairroAndCidade(string $bairro, int $id_cidade)
+    {
+        $sql = "SELECT * FROM logradouro WHERE descricao_bairro = ? AND id_cidade = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $bairro);
+        $stmt->bindValue(2, $id_cidade);
+        $stmt->execute();
+
+        return $stmt->fetchAll(DAO::FETCH_CLASS);
+    }
+}
+
