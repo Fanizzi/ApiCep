38dba31f70e16fb0a41f8ed3a664d9ce29f951b1
Lorenzo Fanizzi <fanizzicontato@gmail.com>
2023-03-16 15:56:25 -0300

    add Funções Controller

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -2,6 +2,8 @@
 
 namespace ApiCep\Controller;
 
+use Exception;
+
 abstract class Controller
 {
     public static function getResponseAsJSON($data)
@@ -13,5 +15,63 @@ abstract class Controller
         header("Pragma: public");
 
         exit(json_encode($data));
+    }
+
+    protected static function getExceptionAsJSON(Exception $e)
+    {
+        $exception = [
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+            'line' => $e->getLine(),
+            'traceAsString' => $e->getTraceAsString(),
+            'previous' => $e->getPrevious()
+        ];
+
+        http_response_code(400);
+
+        header("Acess-Control-Allow-Origin: *");
+        header("Content-type: application/json; charset=utf-8");
+        header("Cache-Control: no-cache, must-revalidate");
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Pragma: public");
+
+        exit(json_encode($exception));
+    }
+
+    protected static function isGet()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'GET')
+        {
+            throw new Exception("O método de requisição deve ser GET");
+        }
+    }
+
+    protected static function isPost()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST')
+        {
+            throw new Exception("O método de requisição deve ser POST");
+        }
+    }
+
+    protected static function getIntFromUr1($var_get, $var_name = null) : int
+    {
+        self::isGet();
+
+        if(!empty($var_get))
+            return (int) $var_get;
+        else
+            throw new Exception("Variável $var_name não identificada.");
+    }
+
+    protected static function getStringFromUr1($var_get, $var_name = null) : string
+    {
+        self::isGet();
+
+        if(!empty($var_get))
+            return (string) $var_get;
+        else
+            throw new Exception("Variável $var_name não identificada.");
     }
 }
